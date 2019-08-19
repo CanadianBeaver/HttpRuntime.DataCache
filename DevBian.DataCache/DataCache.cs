8d9d3750c1a06347cf539f97408b351185058d29
Radu Martin <radu.martin@hotmail.com>
2019-08-19 10:50:37 -0400

    duplicates the methods for increase performance

--- DevBian.DataCache/DataCache.cs
+++ DevBian.DataCache/DataCache.cs
@@ -157,6 +157,47 @@ namespace DevBian.Caching
     }
 
     /// <summary>
+    /// Добавляет объект в глобальный кэш приложений с заданными параметрами кэширования.
+    /// Если объект по такому имени уже существует, то он будет переписан новым значением.
+    /// </summary>
+    /// <param name="cacheName">Имя добавляемого объекта в кэше</param>
+    /// <param name="cacheValue">Значение добавляемого объекта в кэше</param>
+    /// <param name="expirationType">Тип хранения объекта в кэше</param>
+    public static void InsertExpirationData(string cacheName, object cacheValue, CacheExpirationType expirationType)
+    {
+      if (DataCache.IsCacheEnable)
+      {
+        if (expirationType == CacheExpirationType.AbsoluteExpiration)
+          HttpRuntime.Cache.Insert(cacheName, cacheValue, null, DateTime.UtcNow.Add(DataCache.ExpirationTime), Cache.NoSlidingExpiration);
+        else if (expirationType == CacheExpirationType.SlidingExpiration)
+          HttpRuntime.Cache.Insert(cacheName, cacheValue, null, Cache.NoAbsoluteExpiration, DataCache.ExpirationTime);
+        else
+          HttpRuntime.Cache.Insert(cacheName, cacheValue);
+      }
+    }
+    
+    /// <summary>
+    /// Добавляет объект в глобальный кэш приложений с заданными параметрами кэширования.
+    /// Если объект по такому имени уже существует, то он будет переписан новым значением.
+    /// </summary>
+    /// <param name="cacheName">Имя добавляемого объекта в кэше</param>
+    /// <param name="cacheValue">Значение добавляемого объекта в кэше</param>
+    /// <param name="expirationType">Тип хранения объекта в кэше</param>
+    /// <param name="expirationTime">Время хранения объекта в кэше</param>
+    public static void InsertExpirationData(string cacheName, object cacheValue, CacheExpirationType expirationType, TimeSpan expirationTime)
+    {
+      if (DataCache.IsCacheEnable)
+      {
+        if (expirationType == CacheExpirationType.AbsoluteExpiration)
+          HttpRuntime.Cache.Insert(cacheName, cacheValue, null, DateTime.UtcNow.Add(expirationTime), Cache.NoSlidingExpiration);
+        else if (expirationType == CacheExpirationType.SlidingExpiration)
+          HttpRuntime.Cache.Insert(cacheName, cacheValue, null, Cache.NoAbsoluteExpiration, expirationTime);
+        else
+          HttpRuntime.Cache.Insert(cacheName, cacheValue);
+      }
+    }
+
+    /// <summary>
     /// Удаляет объект из глобального кэша приложений по уникальному имени
     /// </summary>
     /// <param name="cacheName">Имя объекта в кэше</param>
